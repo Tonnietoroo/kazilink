9dc29a142f4f5cf08459b873ce493426a000e5e3
Tonnietoroo <toroitichtonny06@gmail.com>
2024-12-18 21:17:23 +0300

    Job profiles

--- kazilink/views.py
+++ kazilink/views.py
@@ -2,18 +2,32 @@ from django.shortcuts import redirect, render
 
 from kazilink.models import Contact
 
+from portal.models import Job, JobProfile
+
 
 def home_page(request):
-    context = {}
-    return render(request, "kazilink/home.html")
+    jobs = Job.objects.filter(approved=True).order_by('-time_created')[:6]  # Fetch top 5 approved jobs
+    context = {
+        'jobs': jobs, 
+    }
+    return render(request, "kazilink/home.html", context)
 
 def about_page(request):
     context = {}
     return render(request, "kazilink/about.html")
 
 def services_page(request):
-    context = {}
-    return render(request, "kazilink/services.html")
+     # Fetch the latest jobs
+    jobs = Job.objects.filter(approved=True).order_by('-time_created')[:6]  # Fetch top 5 approved jobs
+    
+    # Fetch the latest job profiles
+    job_profiles = JobProfile.objects.all().order_by('-id')[:6]  # Fetch top 5 job profiles
+    
+    context = {
+        'jobs': jobs,
+        'job_profiles': job_profiles,
+    }
+    return render(request, 'kazilink/services.html', context)
 
 
 def contact_page(request):
